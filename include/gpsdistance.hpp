21c9ac00766c569ba10563f4e98c42ba98a4d70c
Yukinari Tani <yukinarit84@gmail.com>
2014-08-08 00:45:36 +0900

    templatized LatLng and geometry functions

--- include/gpsdistance.hpp
+++ include/gpsdistance.hpp
@@ -1,28 +1,74 @@
 #ifndef GPSDISTANCE_H
 #define GPSDISTANCE_H
 
+#include <cmath>
+
+// http://www.movable-type.co.uk/scripts/latlong.html
+// https://github.com/johnmeehan/Distance/blob/master/Distance.cpp
+
 namespace gpsdistance
 {
 
 static const double EARTH_RADIUS_KM = 6378.137;
 static const double EARTH_RADIUS_METER = EARTH_RADIUS_KM * 1000.0;
-    
-struct LatLng
+
+
+template <typename T>
+struct LatLng_t
 {
-    LatLng(double latitude, double longitude)
-        : lat(latitude)
-        , lng(longitude)
+    LatLng_t()
+        : lat {}
+        , lng {}
     {}
-    
-    double lat = 0.0;
-    double lng = 0.0;
+
+    LatLng_t(T latitude, T longitude)
+        : lat{latitude}
+        , lng{longitude}
+    {}
+
+    T lat;
+    T lng;
 };
+typedef LatLng_t<double> LatLng;
+
+
+template <typename T>
+T radians(const T& degree)
+{
+    return degree * M_PI / 180.0;
+}
+
+
+template <typename T>
+LatLng_t<T> radians(const LatLng_t<T>& latlng)
+{
+    return LatLng_t<T> {
+        radians(latlng.lat)
+        , radians(latlng.lng)
+    };
+}
     
-double radians(double degree);
-LatLng radians(const LatLng& latlng);
     
-double distance(const LatLng& from, const LatLng& to);
+/*
+ *
+ *
+ */
+template <typename T>
+T distance(const LatLng_t<T>& from, const LatLng_t<T>& to)
+{
+    LatLng_t<T> rfrom = radians(from);
+    LatLng_t<T> rto = radians(to);
+
+    T dlat = rto.lat - rfrom.lat;
+    T dlng = rto.lng - rfrom.lng;
+
+    T a = sin(dlat/2.0)*sin(dlat/2.0) + cos(rfrom.lat) * cos(rto.lat) * sin(dlng/2.0)*sin(dlng/2.0);
+    T c = 2 * atan2(sqrt(a), sqrt(1.0 - a));
+    T d = EARTH_RADIUS_KM * c;
     
-} // gpsdistance
+    return d;
+}
+
+} // namespace gpsdistance
 
 #endif // GPSDISTANCE_H
